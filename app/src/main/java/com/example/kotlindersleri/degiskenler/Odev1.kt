69503005fb57a8bc0a345ac0c5f7063e79db9651
Beyza Başak <bbasakbeyza@gmail.com>
2025-04-25 17:52:46 +0300

    Odev1 çalışmam

---
+++ app/src/main/java/com/example/kotlindersleri/degiskenler/Odev1.kt
@@ -0,0 +1,143 @@
+package com.example.kotlindersleri.degiskenler
+
+fun main() {
+
+    val sehir = "Bursa"
+    println("Şehir : $sehir")
+
+    var ulke = "Fransa"
+    println("Ülke : $ulke")
+
+    val telefon = "0533 333 33 33"
+    println("Telefon : $telefon")
+
+    var posta_kodu = 34500
+    println("Posta kodu : $posta_kodu")
+
+    val email = "ornek@mail.com"
+    println("E-mail : $email")
+
+    var meslek = "Pilot"
+    println("Meslek : $meslek")
+
+    val stok_miktari = 538
+    println("Stok miktarı : $stok_miktari")
+
+    var musteri_adi = "Selma Arslan"
+    println("Müşteri adı : $musteri_adi")
+
+    val bakiye = 1756.89
+    println("Bakiye : $bakiye")
+
+    var dogum_gunu = "21.03.2005"
+    println("Doğum günü : $dogum_gunu")
+
+    val maas = 150000
+    println("Maaş : $maas")
+
+    var medeni_durum = "bekar"
+    println("Medeni durum : $medeni_durum")
+
+    val urun_yorum = "Çok iyi!"
+    println("Ürün yorum : $urun_yorum")
+
+    val odeme_tarihi = "02.06.2025"
+    println("Ödeme tarihi : $odeme_tarihi")
+
+    var odeme = 845
+    println("Ödeme : $odeme")
+
+    val siparis_adeti = 150
+    println("Sipariş adeti : $siparis_adeti")
+
+    var araba_modeli = "Lamborghini Huracan"
+    println("Araba modeli : $araba_modeli")
+
+    val kitap_adi = "Zincir"
+    println("Kitap adı : $kitap_adi")
+
+    var yayinlama_tarihi = "14.08.1974"
+    println("Yayınlama tarihi : $yayinlama_tarihi")
+
+    val indirim_miktari = 50.99
+    println("İndirim miktarı : $indirim_miktari")
+
+    var oda_sayisi = 45
+    println("Oda sayısı : $oda_sayisi")
+
+    val enlem = 15
+    println("Enlem : $enlem")
+
+    val boylam = 23
+    println("Boylam : $boylam")
+
+    var urun_adi = "Kışlık mont"
+    println("Ürün adı : $urun_adi")
+
+    val yemek_fiyati = 330.95
+    println("Yemek fiyatı : $yemek_fiyati")
+
+    var marka = "Mercedes"
+    println("Marka : $marka")
+
+    val muzik_adi = "Zalim"
+    println("Müzik adı : $muzik_adi")
+
+    var video_suresi = 17.56
+    println("Video süresi : $video_suresi")
+
+    var urun_puani = 4.8
+    println("Ürün puanı : $urun_puani")
+
+    var resim_adi = "Teknik resim"
+    println("Resim adı : $resim_adi")
+
+    var dosya_formati = ".pdf"
+    println("Dosya formatı : $dosya_formati")
+
+    var renk = "Kırmızı"
+    println("Renk : $renk")
+
+    var renk_kodu = "#0000"
+    println("Renk kodu : $renk_kodu")
+
+    var telefon_modeli = "Iphone 12"
+    println("Telefon modeli : $telefon_modeli")
+
+    val ekran_boyutu = 17
+    println("Ekran boyutu : $ekran_boyutu")
+
+    val agırlık = 1200
+    println("Ağırlık : $agırlık")
+
+    val ulusal_gun = "23 Nisan"
+    println("Ulusal gün : $ulusal_gun")
+
+    val tatil_gunu = "Cumartesi"
+    println("Tatil günü : $tatil_gunu")
+
+    var rezervasyon_tarihi = "26.04.2025"
+    println("Rezervasyon tarihi : $rezervasyon_tarihi")
+
+    var sokak_adi = "Özlem Sokak"
+    println("Sokak adı : $sokak_adi")
+
+    val otobüs_hatti = "50Z"
+    println("Otobüs hattı : $otobüs_hatti")
+
+    val kalan_dakika = 75
+    println("Kalan dakika : $kalan_dakika")
+
+    var takip_kodu = 124
+    println("Takip kodu : $takip_kodu")
+
+    val kupon_suresi = 5
+    println("Kupon süresi : $kupon_suresi")
+
+    var kupon_kodu = 4533412343
+    println("Kupon kodu : $kupon_kodu")
+
+    val fatura_adresi = "Türkiye/İstanbul ... AŞ."
+    println("Fatura adresi : $fatura_adresi")
+
+}
